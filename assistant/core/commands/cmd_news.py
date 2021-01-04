6463d07aa10a8b6e660046e53a163bbdc6734ed1
Manish kumar thota <37763863+manishkumart@users.noreply.github.com>
2021-01-04 18:43:10 +0530

    Add files via upload

---
+++ assistant/core/commands/cmd_news.py
@@ -0,0 +1,45 @@
+# built-in
+import time
+import requests
+
+from bs4 import BeautifulSoup
+
+# core modules / packages
+from assistant import settings
+from assistant.core.modules import tts, replying
+
+"""
+news
+"""
+def ex(cmd):
+    tts.say(replying.get_reply("news"))
+
+    # check if language is German
+    if settings.LANGUAGE == "en":
+        url = "https://indianexpress.com/"
+    # else use English
+    else:
+        url = "https://news.google.com/?hl=en-US&gl=US&ceid=US:en"
+    
+    # get the source code from the url
+    source_code = requests.get(url).text
+    # create a bs4 object
+    soup = BeautifulSoup(source_code, features="html.parser")
+
+    # the news dict
+    news = []
+
+    class_name = "ipQwMb ekueJc RD0gLb"
+    # find every item with thegiven class name
+    tags = soup.find_all("h3", {"class": class_name})
+
+    tts.say(replying.get_reply("news", stage=1))
+
+    ticks = 0
+    # read every top title
+    for tag in tags:
+        ticks += 1
+        title = tag.getText()
+        tts.say(title)
+        if ticks >= settings.MAX_NEWS_TICKS:
+            break
---
+++ assistant/core/commands/cmd_weather.py
@@ -0,0 +1,36 @@
+# built-in
+import json
+import requests
+
+# core
+from assistant import settings
+# core modules / packages
+from assistant.core.modules import log, tts, replying
+
+"""
+weather
+says the todays'
+weather
+"""
+def ex(cmd):
+    # weather request url
+    url = "http://api.openweathermap.org/data/2.5/weather?q={0}&appid={1}&lang={2}&units=metric".format(settings.LOCATION, settings.WEATHER_API_KEY, settings.LANGUAGE_SHORT)
+
+    # get the source code from the url
+    data = requests.get(url)
+    # convert json
+    content = json.loads(data.text)
+
+    # get data and
+    # test if there's an error message
+    if "message" in content:
+        log.error(content["message"])
+        tts.say(content["message"])
+    # or get real data
+    else:
+        temp = content["main"]["temp"]
+        temp_max = content["main"]["temp_max"]
+        desc = content["weather"][0]["description"]
+
+        tts.say(replying.get_reply("weather").format(
+            settings.LOCATION, temp, desc, temp_max))
